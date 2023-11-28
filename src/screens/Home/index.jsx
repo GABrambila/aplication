647a36039f8812e0494388cef12a6d81ca29c1ba
ShaolinMatadorDePorco22 <107507857+ShaolinMatadorDePorco22@users.noreply.github.com>
2023-11-28 11:49:38 -0300

    commit eduardo

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, PermissionsAndroid, Text } from 'react-native';
+import { View, PermissionsAndroid, Text, TouchableOpacity } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import * as Location from "expo-location"
@@ -7,10 +7,11 @@ import * as Permissions from "expo-permissions"
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import config from "../../../config/index.json"
 import MapViewDirections from "react-native-maps-directions"
+import styles from './styles';
 
 
 export default function Home() {
-  const mapEl=useRef(null)
+
   const [origin, setOrigin] = useState(null)
   const [destination, setDestination] = useState(null)
   const [distance, setDistance] = useState(null)
@@ -35,23 +36,26 @@ export default function Home() {
     <View style={{ flex: 1 }}>
 
       <MapView
-        style={{ width: 300, height: 300 }}
+        style={{ width: '100%', height: 300 }}
         initialRegion={origin}
         showsUserLocation={true}
-        ref={mapEl}
+  
       >
-        {destination && 
+        {destination &&
           <MapViewDirections
-          origin={origin}
-          destination={destination}
-          apikey={config.googleApi}
-          strokeWidth={3}
-          onReady={result=>{
-            setDistance(result.distance);
-            setPrice(result.distance * 3);
-          }}
-        ></MapViewDirections>}
-        
+            
+            origin={origin}
+            destination={destination}
+            apikey={config.googleApi}
+            strokeWidth={3}
+            onReady={result => {
+            
+              setDistance(result.distance.toFixed(2).replace('.', ','));
+              setPrice(result.distance.toFixed(2) * 4);
+
+            }}
+          ></MapViewDirections>}
+
 
       </MapView>
       <View>
@@ -64,25 +68,29 @@ export default function Home() {
               latitudeDelta: 0.000922,
               longitudeDelta: 0.000421
             })
-            console.log(destination)
+            console.log([destination])
           }}
           query={{
             key: config.googleApi,
             language: 'pt-br',
           }}
           fetchDetails={true}
-          styles={{listView:{position:'absolute', top:40, borderLeftWidth:1, borderRightWidth:1, borderBottomWidth:1 ,height:100, width:100}}}
+          styles={{ listView: { borderLeftWidth: 1, borderRightWidth: 1, borderBottomWidth: 1 }, container: { position: 'absolute', width: '100%' } }}
         />
 
-       
-          {distance&&
-           <View>
-              <Text>Distancia:{distance}m</Text>
-              <Text>Preco: {price}</Text>
-            </View>
-          }
-        
+
+
+
       </View>
+
+      {distance &&
+        <View style={styles.containerPrice}>
+          <Text style={styles.distanceTxt}>Distancia:{distance}km</Text>
+          <TouchableOpacity style={styles.priceBtn}>
+            <Text style={styles.priceTxt}>Preco:R$ {price}</Text>
+          </TouchableOpacity>
+        </View>
+      }
 
     </View>
   );
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    containerPrice:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        padding:10,
+        
+    },
+    distanceTxt:{
+        fontSize:20,
+        fontWeight:'bold'
+    },
+    priceBtn:{
+        backgroundColor:'black',
+        padding:7,
+        borderRadius:5,
+        marginTop:30,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    priceTxt:{
+        color:'#fff',
+        fontWeight:'bold',
+        fontSize:20
+    }
+});
+ export default styles
